ef0a4869558dd7632c71dd13538cb90ff463f054
Victor Yamchinov <kodiers@gmail.com>
2019-01-08 18:30:22 +0300

    added middleware

--- polls/main.py
+++ polls/main.py
@@ -5,11 +5,13 @@ import jinja2
 from polls.settings import config
 from polls.db import init_pg, close_pg
 from polls.routes import setup_routes
+from polls.middlewares import setup_middlewares
 
 
 app = web.Application()
 aiohttp_jinja2.setup(app=app, loader=jinja2.PackageLoader('polls', 'templates'))
 setup_routes(app)
+setup_middlewares(app)
 app['config'] = config
 app.on_startup.append(init_pg)
 app.on_cleanup.append(close_pg)
